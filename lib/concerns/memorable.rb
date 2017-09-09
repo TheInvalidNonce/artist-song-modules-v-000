a7e43ac75ab8e414786566949e660c3a5ac00ae4
Janusz Szubert <theinvalidnonce@gmail.com>
2017-09-09 19:53:07 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -3,15 +3,15 @@ module Memorable
     def reset_all
       self.all.clear
     end
- 
+
     def count
       self.all.count
     end
   end
- 
+
   module InstanceMethods
     def initialize
-      # some more code coming soon!
+      self.class.all
     end
   end
 end

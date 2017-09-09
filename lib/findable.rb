8de35f20480ee0a5d792ad7372a199fc01c31195
Janusz Szubert <theinvalidnonce@gmail.com>
2017-09-09 19:56:04 +0000

    automatically backed up by learn

--- lib/findable.rb
+++ lib/findable.rb
@@ -3,7 +3,7 @@ module Findable
   module ClassMethods
 
     def self.find_by_name(name)
-      self.class.all.detect{|a| a.name == name}
+      self.all.detect{|a| a.name == name}
     end
 
   end
